d1b88fcdd6399dac8e85f8e76c573f269ad2b6a5
PasinduChathura <71862854+PasinduChathura@users.noreply.github.com>
2025-02-27 12:16:45 +0530

    Update create_triggers.sql

--- create_triggers.sql
+++ create_triggers.sql
@@ -3,25 +3,21 @@ ON Transactions
 AFTER INSERT
 AS
 BEGIN
-	INSERT INTO TranSummary(id, originId, paymentMode, custMobile, transType, cardLabel, traceNo, invoiceNo, amount, currency, batchNo, pan, merchantId, terminalId, dateTime, expDate, nii, rrn, authCode, signData, tipAmount, entryMode, dccCurrency, dccTranAmount, lat, lng, isAway, createdAt, updatedAt)
-	SELECT id, originId, paymentMode, custMobile, transType, cardLabel, traceNo, invoiceNo, amount, currency, batchNo, pan, merchantId, terminalId, dateTime, expDate, nii, rrn, authCode, signData, tipAmount, entryMode, dccCurrency, dccTranAmount, lat, lng, isAway, createdAt, updatedAt
-	FROM INSERTED;
+    INSERT INTO TranSummary (originId, paymentMode, custMobile, transType, cardLabel, traceNo, invoiceNo, amount, currency, batchNo, pan, merchantId, terminalId, dateTime, expDate, nii, rrn, authCode, signData, tipAmount, entryMode, dccCurrency, dccTranAmount, lat, lng, isAway, createdAt, updatedAt)
+    SELECT originId, paymentMode, custMobile, transType, cardLabel, traceNo, invoiceNo, amount, currency, batchNo, pan, merchantId, terminalId, dateTime, expDate, nii, rrn, authCode, signData, tipAmount, entryMode, dccCurrency, dccTranAmount, lat, lng, isAway, createdAt, updatedAt
+    FROM INSERTED;
 END;
+
 
 CREATE TRIGGER tranTriggerUpdate
 ON Transactions
 AFTER UPDATE
 AS
 BEGIN
-  UPDATE TranSummary
-  SET transType = (SELECT transType FROM INSERTED), 
-      updatedAt = (SELECT updatedAt FROM INSERTED),
-      isAway = (SELECT isAway FROM INSERTED)
-  WHERE id = (SELECT id FROM INSERTED);
+    UPDATE TranSummary
+    SET TranSummary.transType = i.transType, 
+        TranSummary.updatedAt = i.updatedAt,
+        TranSummary.isAway = i.isAway
+    FROM TranSummary t
+    INNER JOIN INSERTED i ON t.id = i.id;
 END;
-
-UPDATE TranSummary 
-    SET signData = (SELECT signData
-        FROM Transactions
-        WHERE TranSummary.id = Transactions.id
-    );
